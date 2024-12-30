3023fa83fb10b46577a7979e31a458ef4bf561e6
Sayuru Priyanjana <sayurupriyanjana41@gmail.com>
2024-12-31 04:16:34 +0530

    Heroheader  upto parallax effect

--- java-haven-web/src/App.jsx
+++ java-haven-web/src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './components/Navbar/navbar'
 import './App.css'
 import HeroHeader from './components/HeroHeader/HeroHeader'
 
+
 function App() {
   return (
     <div className="app-container">
